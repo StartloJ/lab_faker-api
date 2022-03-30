d52060577609ce4e1835613c2cf674035d123da9
Watcharin YangNgam <watcharin@opsta.co.th>
2022-03-30 13:25:49 +0700

    update script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/ash
 
-./init.sh
-
 run_port=${PORT:-'3000'}
-run_host=${HOST:-'0.0.0.0'}
+run_host=${HOST:-'127.0.0.1'}
 
-json-server --host $run_host --port $run_port db.json
+json-server --host $run_host --port $run_port --routes /app/route.json /app/db.json
